ca1690f43093cf4e2ae164c73e5c99e9a1606e59
Markus Lippitz <markus.lippitz@uni-bayreuth.de>
2024-07-04 11:03:26 +0200

    2D updates

--- 3_nonlinear/4_2d_spec/pluto/liouville_tracks.jl
+++ 3_nonlinear/4_2d_spec/pluto/liouville_tracks.jl
@@ -1,105 +1,63 @@
 ### A Pluto.jl notebook ###
-# v0.15.1
+# v0.19.42
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ 6dd2e2dc-3567-4de4-950f-f43df760453b
 # the code requires the following structure for a 2-sided Feynman diagram
-
-number_of_mu = 4
+number_of_mu = 4;
 
 # ╔═╡ 52e82dce-1218-4c4c-a745-e9c053fd1642
-mutable struct Interaction
-	column
-	direction
+begin
+	@enum Side left right
+	@enum Direction inward outward
+	
+	mutable struct Interaction
+		side :: Side
+		direction :: Direction
+	end
+	
+	mutable struct State
+			bra :: Int 
+			ket :: Int
+	end
+
+	mutable struct Track
+		states :: Array{State, 1}
+		interaction :: Array{Interaction,1}
+	end
 end
-
-# ╔═╡ efb61be9-c2c7-436c-9109-70f9d5103c9c
-interaction = Interaction(zeros(Float64, number_of_mu +1), zeros(Float64, number_of_mu +1))
-
-# ╔═╡ 055ef168-4d4a-4a23-ac18-3e523263af12
-mutable struct Track
-	states
-	interaction
-	phasematch
-end
-
-# ╔═╡ bedf677d-c929-4ef5-830d-6f8e2f87e670
-track = Track(zeros(Float64, number_of_mu +1,2) ,interaction,zeros(Float64, number_of_mu +1,2))
 
 # ╔═╡ 7e388706-bb14-461f-b3e1-a0213a596125
-# index in first dimension is "running number of interaction + 1"
-# index in second dimension is column: 1= left = ket; 2 = right = bra
-# value is number of state = quanmtum number; but >=1 
-# this number is reduced by 1 for display !
+begin
+		track = Track(
+			Array{State, 1}(undef, number_of_mu +1) ,
+			Array{Interaction,1}(undef, number_of_mu + 1)
+	)
+	
+		# interaction between step A and B is at index position of B
+		
+		# we start from ground state 
+		track.states[1] =  State(0,0)  
+	
+		# 1. interaction: absorption into left
+		track.interaction[2] = Interaction(left, inward)
+		track.states[2] = State(1,0)  ;  # incremented quantum number
 
-# we start from ground state 
-track.states[1,[1,2]] .= 1;  #
+		# 2. interaction: absorption into right
+		track.interaction[3]  = Interaction(right, inward);  
+		track.states[3] = State(1,1)  ;  # incremented quantum number
 
-# ╔═╡ 208c8f9a-f79d-4c0a-8b2c-5e263484b93d
-# 1. interaction: absorption into left
-track.interaction.column[2]  = 1;  # = left
+		# 3. interaction: emission from right 
+		track.interaction[4]  = Interaction(right, outward);  
+		track.states[4] = State(1,0)  ;  # decremented quantum number
 
-# ╔═╡ 5054a8ff-8c63-4a43-bfd2-dca814592cbe
-track.interaction.direction[2]  = 1;  # = absorption
+		# 4. interaction = radiation polarization: emission from left 
+		track.interaction[5]  = Interaction(left, outward);  
+		track.states[5] = State(0,0)  ;  # decremented quantum number
 
-# ╔═╡ e07a2266-8292-440c-b20f-22fcdfa0bf98
-track.phasematch[2]  = 1;  # left-to-right
-
-# ╔═╡ 2da196be-5631-41e1-afd4-80537b51b3b8
-track.states[2,1] = 2;  # incremented quantum number
-
-# ╔═╡ a3e6807c-9102-4f79-aaed-76a2a4684c6a
-track.states[2,2] = 1;
-
-# ╔═╡ bd25e871-8ec5-4727-ab58-f1283e56cdaa
-# 2. interaction: absorption into right
-track.interaction.column[3]  = 2;  # = right
-
-# ╔═╡ 79524a19-358c-406b-98a9-b1e0bebfc07c
-track.interaction.direction[3]  = 1;  # = absorption
-
-# ╔═╡ 8ca50cdd-8945-4099-b07c-a3f1b4986300
-track.phasematch[3]  = -1;  # right-to-left
-
-# ╔═╡ d82f5002-7dfe-44e4-8058-753ffcf4e87e
-track.states[3,1] = 2;  # incremented quantum number
-
-# ╔═╡ e907721d-9ee0-4514-9778-d5a904d8033d
-track.states[3,2] = 2;
-
-# ╔═╡ 7aa68c5f-d3d0-485b-b576-121e77c915dd
-# 3. interaction: emission from right 
-track.interaction.column[4]  = 2;  # = right
-
-# ╔═╡ 2e2da54a-0eaa-4414-95a1-dc283b28bc5c
-track.interaction.direction[4]  = -1;  # = emission
-
-# ╔═╡ bd572b51-0978-41ba-bd17-00298f67e9d7
-track.phasematch[4]  = 1;  # left-to-right
-
-# ╔═╡ d3eb6360-4682-4c85-8d68-6d76536c2fb9
-track.states[4,1] = 2;
-
-# ╔═╡ ce83a45d-135e-463f-b9a8-812e012a84bd
-track.states[4,2] = 1;  # decremented quantum number
-
-# ╔═╡ 549c5f14-70e1-44b1-9b0d-c60fabac5295
-# 3. interaction = radiation polarization: emission from left 
-track.interaction.column[5]  = 1;  # = left
-
-# ╔═╡ 5cc2a168-9ca2-42fa-9cdb-c3cba0ce751b
-track.interaction.direction[5]  = -1;  # = emission
-
-# ╔═╡ ef95be20-0291-4721-a67e-03ba032cac28
-track.phasematch[5]  = -1;  # right-to-left
-
-# ╔═╡ ad8bbcff-147e-4bf1-a59d-5ed309697f19
-track.states[5,1] = 1;  # decremented quantum number
-
-# ╔═╡ 7d2ab482-a91d-48bb-a070-e8fbafcb64d7
-track.states[5,2] = 1;
+end;
 
 # ╔═╡ d4aae981-626b-46ee-be10-08881943b4c8
 # of course on better does this with a script. Your turn ...
@@ -108,43 +66,125 @@ track.states[5,2] = 1;
 
 tracks = [track]
 
-# ╔═╡ 5660424e-b3bb-4b26-beef-89f4acd65d42
-# display all tracks; grouped by phase matching
-for id1=1:2
-    for id2=1:2
-        for id3=1:2
-            pm = ([id1 id2 id3]' - 1.5) .* 2
-            display_tracks_by_phasematch[tracks,pm]
+# ╔═╡ 3d89dfb3-7788-4ce3-b395-90137d8a8ceb
+function calc_phasematch(interaction::Interaction)
+	if ( (interaction.side == left)  & (interaction.direction == inward)) |
+		((interaction.side == right) & (interaction.direction == outward))
+		pm = '+'
+	else
+		pm = '-'
+	end
+	return pm
+end
+
+# ╔═╡ 2e706899-ced8-4f23-b654-890832704502
+function display_track(tracks)
+    # displays tracks as double-sided Feynman diagrams
+    # all tracks have to fit in one line !
+
+    s = size(tracks[1].states)
+
+    # print double sided Feynman diag.
+    # from top to bottom = backwards in time
+    for id in reverse(1:s[1])
+        for id2 in 1:length(tracks)
+			if (id > 1)
+		            interaction = tracks[id2].interaction[id].direction == inward ? "+" : "-"
+			
+		            if tracks[id2].interaction[id].side == left
+		                sl = interaction
+		                sr = " "
+		            else
+		                sl = " "
+		                sr = interaction
+		            end
+			else
+				sl = " "
+				sr = " "
+			end
+
+			a = tracks[id2].states[id].bra
+			b = tracks[id2].states[id].ket  
+			
+            print("$sl| $(a) $(b) |$sr       ")
         end
+        println()
     end
+
+    # print phase matching signs
+    print("   ")
+    for id2 in 1:length(tracks)
+        for id in 2:s[1]
+            print(calc_phasematch(tracks[id2].interaction[id]))
+        end
+        print("            ")
+    end
+    println()
+end
+
+# ╔═╡ f750e54f-b178-4279-b950-3002e7ea0ba2
+display_track(tracks)
+
+# ╔═╡ 11a17f82-c76d-4f33-86d3-57117ce0afe2
+function select_by_phasematch(tracks, pm)
+    # select those tracks that fulfill phasematching condition
+    # pm: string of * / -
+    
+    n = length(pm)
+    
+    ok = falses(length(tracks))
+    for id in 1:length(tracks)
+        ok[id] = (join(calc_phasematch.(tracks[id].interaction[2:end])) == pm)
+    end
+    
+    return ok
+end
+
+# ╔═╡ 39b85b65-b020-476f-aede-6b5bfcd5114e
+function display_tracks_by_phasematch(tracks, phasematch)
+    # select the tracks
+    tr = tracks[select_by_phasematch(tracks, phasematch)]
+
+    # distribute tracks in lines of 4
+    tracks_per_line = 4
+    n = floor(Int, length(tr) / tracks_per_line)
+    rem = length(tr) - n * tracks_per_line
+
+    for id in 0:(n-1)
+        display_track(tr[(id*tracks_per_line +1):(id*tracks_per_line + tracks_per_line)])
+        println()
+    end
+
+    if (rem > 0)
+        display_track(tr[end-rem+1:end])
+    end
+    
+    println("\n")
+end
+
+# ╔═╡ 5660424e-b3bb-4b26-beef-89f4acd65d42
+begin
+	# display all tracks; grouped by phase matching
+	
+	signs = ['+','-']
+	for id1 in signs
+	    for id2 in signs
+	        for id3 in signs
+	            pm = join([id1 , id2,  id3, '-'] )  # last is always -
+	            display_tracks_by_phasematch(tracks,pm)
+	        end
+	    end
+	end
 end
 
 # ╔═╡ Cell order:
 # ╠═6dd2e2dc-3567-4de4-950f-f43df760453b
 # ╠═52e82dce-1218-4c4c-a745-e9c053fd1642
-# ╠═efb61be9-c2c7-436c-9109-70f9d5103c9c
-# ╠═055ef168-4d4a-4a23-ac18-3e523263af12
-# ╠═bedf677d-c929-4ef5-830d-6f8e2f87e670
 # ╠═7e388706-bb14-461f-b3e1-a0213a596125
-# ╠═208c8f9a-f79d-4c0a-8b2c-5e263484b93d
-# ╠═5054a8ff-8c63-4a43-bfd2-dca814592cbe
-# ╠═e07a2266-8292-440c-b20f-22fcdfa0bf98
-# ╠═2da196be-5631-41e1-afd4-80537b51b3b8
-# ╠═a3e6807c-9102-4f79-aaed-76a2a4684c6a
-# ╠═bd25e871-8ec5-4727-ab58-f1283e56cdaa
-# ╠═79524a19-358c-406b-98a9-b1e0bebfc07c
-# ╠═8ca50cdd-8945-4099-b07c-a3f1b4986300
-# ╠═d82f5002-7dfe-44e4-8058-753ffcf4e87e
-# ╠═e907721d-9ee0-4514-9778-d5a904d8033d
-# ╠═7aa68c5f-d3d0-485b-b576-121e77c915dd
-# ╠═2e2da54a-0eaa-4414-95a1-dc283b28bc5c
-# ╠═bd572b51-0978-41ba-bd17-00298f67e9d7
-# ╠═d3eb6360-4682-4c85-8d68-6d76536c2fb9
-# ╠═ce83a45d-135e-463f-b9a8-812e012a84bd
-# ╠═549c5f14-70e1-44b1-9b0d-c60fabac5295
-# ╠═5cc2a168-9ca2-42fa-9cdb-c3cba0ce751b
-# ╠═ef95be20-0291-4721-a67e-03ba032cac28
-# ╠═ad8bbcff-147e-4bf1-a59d-5ed309697f19
-# ╠═7d2ab482-a91d-48bb-a070-e8fbafcb64d7
 # ╠═d4aae981-626b-46ee-be10-08881943b4c8
 # ╠═5660424e-b3bb-4b26-beef-89f4acd65d42
+# ╠═f750e54f-b178-4279-b950-3002e7ea0ba2
+# ╠═2e706899-ced8-4f23-b654-890832704502
+# ╠═39b85b65-b020-476f-aede-6b5bfcd5114e
+# ╠═11a17f82-c76d-4f33-86d3-57117ce0afe2
+# ╠═3d89dfb3-7788-4ce3-b395-90137d8a8ceb
